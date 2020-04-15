0ad0e96a0f35150d5afc5fc995d000e6c006f301
HuataoWen421 <60019549+HuataoWen421@users.noreply.github.com>
2020-04-15 16:00:28 -0700

    newsapp finish searview

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,19 +1,56 @@
 package com.example.newsapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ActionBar;
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+//import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
@@ -21,10 +58,26 @@ public class MainActivity extends AppCompatActivity {
     List<Fragment> fragments;
     MenuItem menuItem;
 
+    public ArrayAdapter<String> newsAdapter;
+    private RequestQueue mRequestQueue;
+    private Handler handler;
+    private static final int TRIGGER_AUTO_COMPLETE = 100;
+    private static final long AUTO_COMPLETE_DELAY = 300;
+    private String keyword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        //getSupportActionBar().setElevation(0);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_search_black_24dp);
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        mRequestQueue = Volley.newRequestQueue(this);
 
         // Init view
         viewPager = findViewById(R.id.viewPager);
@@ -109,5 +162,112 @@ public class MainActivity extends AppCompatActivity {
         public int getCount() {
             return fragments.size();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the search menu action bar.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.search_main, menu);
+
+        // Get the search menu.
+        MenuItem searchMenu = menu.findItem(R.id.action_search);
+
+        // Get SearchView object.
+        SearchView searchView = (SearchView) searchMenu.getActionView();
+        //EditText searchEditText = (EditText) searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        //searchEditText.setTextColor(getResources().getColor(R.color.black));
+        //searchEditText.setHintTextColor(getResources().getColor(R.color.gray));
+
+
+        // Get SearchView autocomplete object.
+        final SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchAutoComplete.setDropDownBackgroundResource(android.R.color.white);
+        searchAutoComplete.setThreshold(1);
+
+        // Create a new ArrayAdapter and add data to search auto complete object.
+        newsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line);
+        searchAutoComplete.setAdapter(newsAdapter);
+
+        // Listen to search view item on click event.
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int itemIndex, long id) {
+                String queryString = (String) adapterView.getItemAtPosition(itemIndex);
+                searchAutoComplete.setText("" + queryString);
+                Toast.makeText(MainActivity.this, "you clicked " + queryString, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if (msg.what == TRIGGER_AUTO_COMPLETE) {
+                    parseJSOn();
+                }
+                return false;
+            }
+        });
+
+        // Below event is triggered when submit search query.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Start new activity
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setMessage("Search keyword is " + query);
+                alertDialog.show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() > 3) {
+                    keyword = newText;
+                    handler.removeMessages(TRIGGER_AUTO_COMPLETE);
+                    handler.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE, AUTO_COMPLETE_DELAY);
+                }
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void parseJSOn() {
+        String url = "https://xiaobudai.cognitiveservices.azure.com/bing/v7.0/suggestions?mkt=en-US&q=" + keyword;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("suggestionGroups")
+                            .getJSONObject(0)
+                            .getJSONArray("searchSuggestions");
+                    newsAdapter.clear();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject hit = jsonArray.getJSONObject(i);
+                        newsAdapter.add(jsonArray.getJSONObject(i).getString("displayText"));
+                    }
+                    newsAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Ocp-Apim-Subscription-Key", "8cfdb72a5dfe44ba94f7d66f8a598f0a");
+                return headers;
+            }
+        };
+
+        mRequestQueue.add(request);
     }
 }
